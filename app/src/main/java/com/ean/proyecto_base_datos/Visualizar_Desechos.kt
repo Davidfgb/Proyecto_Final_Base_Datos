a4379b36e56747eb545ae7ff565e42c6c394bdfb
Davidfgb <98990796+Davidfgb@users.noreply.github.com>
2022-11-16 20:32:18 -0500

    ingr_info_listo

--- app/src/main/java/com/ean/proyecto_base_datos/Visualizar_Desechos.kt
+++ app/src/main/java/com/ean/proyecto_base_datos/Visualizar_Desechos.kt
@@ -23,45 +23,40 @@ class Visualizar_Desechos : AppCompatActivity() {
         val bundle = intent.extras
         val id_gmail = bundle?.getString("correo")
 
-
-
-
-
-
         val collect = id_gmail.toString()
 
 
-        val sdf = SimpleDateFormat("M/yyyy")
+        val sdf = SimpleDateFormat("yyyy")
         val currentDate = sdf.format(Date())
+
+        val facha_m= SimpleDateFormat("M")
+        val mes = sdf.format(Date())
 
 
         Toast.makeText(baseContext, collect, Toast.LENGTH_SHORT).show()
 
-
         val total = findViewById<TextView>(R.id.editText_visu_total)
 
         //total.setText(documento)
-
-        
-        val docRef = db.collection(collect).document(currentDate)
-        docRef.get()
-            .addOnSuccessListener { document ->
-                if (document != null) {
-                    Log.d(ContentValues.TAG, "DocumentSnapshot data: ${document.data}")
-                    total.setText("${document.data}")
-                } else {
-                    Log.d(ContentValues.TAG, "No such document")
+        try {
+            val docRef = db.collection("desechos").document(currentDate)
+            docRef.get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        Log.d(ContentValues.TAG, "DocumentSnapshot data: ${document.data}")
+                        total.setText("${document.data}")
+                    } else {
+                        Log.d(ContentValues.TAG, "No such document")
+                    }
                 }
-            }
-            .addOnFailureListener { exception ->
-                Log.d(ContentValues.TAG, "get failed with ", exception)
-            }
+                .addOnFailureListener { exception ->
+                    Log.d(ContentValues.TAG, "get failed with ", exception)
+                }
+        }
+        catch (e: Exception) {
+            Toast.makeText(baseContext, "Ese mes o año todavia no a sido ingresado", Toast.LENGTH_SHORT).show()
 
-
-
-
-
-
+        }
 
     }
 }
